e751ba9a4e71802807068b897bc75b6499fa866b
joaogamin <joaumxp@hotmail.com>
2024-10-10 12:46:35 -0300

    jgamin : feat add init_db

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+def init_db():
+	inspector = db.inspect(db.engine)
+	tables = inspector.get_table_names()
+
+	if not tables:
+		db.create_all()
+		db.session.commit()
+		print("Database tables created successfully!")
+	else:
+		print("Tables already exist. No action needed.")
