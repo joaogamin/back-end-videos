7b64b4c87f3c0995bd9319fa35450bb0a5ad9cca
joaogamin <joaumxp@hotmail.com>
2024-10-10 15:53:49 -0300

    jgamin : feat update video module to include all controllers

--- controllers/video/__init__.py
+++ controllers/video/__init__.py
@@ -1,1 +1,2 @@
-from . import create_video_controller
+from . import create_video_controller, update_video_controller, delete_video_controller, get_all_videos_controller, \
+	get_video_by_id_controller
