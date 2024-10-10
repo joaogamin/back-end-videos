c2c60e782f23f9d92a77766deab4b1791d36a9eb
joaogamin <joaumxp@hotmail.com>
2024-10-10 15:34:01 -0300

    jgamin : feat change to postegreSQL

--- database.py
+++ database.py
@@ -4,12 +4,17 @@ db = SQLAlchemy()
 
 
 def init_db(database: SQLAlchemy):
+	import models.video
+
 	inspector = database.inspect(database.engine)
 	tables = inspector.get_table_names()
 
 	if not tables:
+		models.video
+
 		database.create_all()
 		database.session.commit()
+
 		print("Database tables created successfully!")
 	else:
 		print("Tables already exist. No action needed.")
