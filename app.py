344434720e7f66883e90d61c2bb95484fe1e915d
joaogamin <joaumxp@hotmail.com>
2024-10-10 12:02:35 -0300

    jgamin : feat add hello_world route

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+	res = make_response({"status": 200, "message": "Hello World!"}, 200)
+	
+	return res
