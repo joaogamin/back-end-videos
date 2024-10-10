c67cf605f564ec7c8f787aae0d627df133b5245b
joaogamin <joaumxp@hotmail.com>
2024-10-10 12:53:37 -0300

    jgamin : feat add database to app

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+import os
+
 from flask import Flask, make_response
 
+from database import db, init_db
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+db.init_app(app)
+
+with app.app_context():
+	init_db(db)
 
 
 @app.route("/")
 def hello_world():
 	res = make_response({"status": 200, "message": "Hello World!"}, 200)
-	
+
 	return res
--- database.py
+++ database.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def init_db():
-	inspector = db.inspect(db.engine)
+def init_db(database: SQLAlchemy):
+	inspector = database.inspect(database.engine)
 	tables = inspector.get_table_names()
 
 	if not tables:
-		db.create_all()
-		db.session.commit()
+		database.create_all()
+		database.session.commit()
 		print("Database tables created successfully!")
 	else:
 		print("Tables already exist. No action needed.")
