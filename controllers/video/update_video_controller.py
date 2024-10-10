b8ecbb9f44deba1027493ae41cf7a4dab5125961
joaogamin <joaumxp@hotmail.com>
2024-10-10 15:47:50 -0300

    jgamin : feat add update_video_controller

---
+++ controllers/video/update_video_controller.py
@@ -0,0 +1,32 @@
+from flask import make_response, request
+
+from app import app
+from models.video import Video
+from services.Videos.update_video_service import update_video_service
+from utils.get_youtube_video_metadata import get_youtube_video_metadata
+
+
+@app.route('/videos/<video_id>', methods=["PATCH"])
+def update_video_controller(video_id):
+	data = request.json
+	id = data.get("id")
+
+	if not id:
+		return make_response({"message": "Video-Id not specified", "status": 400}, 400)
+
+	video_data = get_youtube_video_metadata(id)
+
+	if not video_data:
+		return make_response({"status": 404, "message": f"Video with id = {id}, not found on youtube"}, 404)
+
+	video = Video(id=video_data["id"], title=video_data["title"], channel=video_data["channel"], duration=["duration"])
+
+	response = update_video_service(video_id, video)
+
+	if response == 404:
+		return make_response({"status": 404, "message": f"Video with id = {id}, not found"}, 404)
+
+	if response == 409:
+		return make_response({"status": 409, "message": f"Video with id = {id}, already exists"})
+
+	return make_response({"status": 200}, 200)
--- services/Videos/update_video_service.py
+++ services/Videos/update_video_service.py
@@ -1,19 +1,21 @@
+from typing import Literal
+
 from sqlalchemy import update
 
 from database import db
 from models.video import Video
 
 
-def update_video_service(id: str, video: Video) -> None | Video:
+def update_video_service(id: str, video: Video) -> Literal[404, 409] | Video:
 	video_exists = Video.query.get(id)
 
 	if not video_exists:
-		return None
+		return 404
 
 	new_video_already_exists = Video.query.get(video.id)
 
 	if new_video_already_exists:
-		return None
+		return 409
 
 	updated_video = db.session.execute(update(Video).where(Video.id == id).values(
 		id=video.id,
